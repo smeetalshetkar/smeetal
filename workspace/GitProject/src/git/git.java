860cef3d6dea3c75031fc496537451153f90586f
smeetal_shetkar <smeetal_shetkar@persistent.co.in>
2014-01-01 14:08:28 +0530

    Committing changes by smeetal

---
+++ workspace/GitProject/src/git/git.java
@@ -0,0 +1,13 @@
+package git;
+
+public class git {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git is fun");
+	}
+
+}
