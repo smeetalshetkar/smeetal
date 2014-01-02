911a6360804de82a2765df1e2991577dc9d0b8f1
smeetal_shetkar <smeetal_shetkar@persistent.co.in>
2014-01-02 11:07:18 +0530

    "Commiting to jenkins without packages"

---
+++ workspace/TestGit/src/Testgit.java
@@ -0,0 +1,14 @@
+
+public class Testgit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5,b=8;
+		int c=a+b;
+		System.out.println("Sum is "+c);
+	}
+
+}
