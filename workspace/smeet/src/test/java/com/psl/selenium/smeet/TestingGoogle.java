40dba077e1305266a9dc50c031e935e5ede70f72
smeetal_shetkar <smeetal_shetkar@persistent.co.in>
2014-01-09 14:10:02 +0530

    commit pooja

--- workspace/smeet/src/test/java/com/psl/selenium/smeet/TestingGoogle.java
+++ workspace/smeet/src/test/java/com/psl/selenium/smeet/TestingGoogle.java
@@ -1,29 +1,27 @@
 package com.psl.selenium.smeet;
 
 import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertTrue;
+
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.*;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
-import org.junit.AfterClass;
+
 import org.junit.Before;
-import org.junit.BeforeClass;
+
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class TestingGoogle {
 	// private String baseUrl;
 	//  private WebDriver driver;
